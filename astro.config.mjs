b4c955c41c4d888ba77cb55badf8643ef85219d2
yxsifdev <yosifblancotafur2006@gmail.com>
2024-11-19 10:41:20 -0500

    Acciones

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://yxsifdev.github.io",
+  integrations: [tailwind()],
+});
