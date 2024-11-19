d7e2e080fbecc602d780d1f895b330c7dec6ab3f
yxsifdev <yosifblancotafur2006@gmail.com>
2024-11-19 15:05:20 -0500

    Update

---
+++ src/consts/socialLinks.ts
@@ -0,0 +1,13 @@
+interface SocialProps {
+  Icon: any;
+  name: string;
+  url: string;
+}
+
+export const socialLinks: SocialProps[] = [
+  {
+    Icon: "st",
+    name: "Discord",
+    url: "https://twitch.tv/cap",
+  },
+];
